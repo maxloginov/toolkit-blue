c3477644a4aaef8d3170ca12dd6a51bcadd7dbb3
Max Loginov <info@maxloginov.com>
2016-06-23 19:57:57 -0700

    Added index.js to allow 'require' of the package

---
+++ index.js
@@ -0,0 +1,6 @@
+// Essential includes
+require('./theme-components/bootstrap3.js');
+
+// Theme fonts
+require('font-awesome/less/font-awesome.less');
+require('material-design-iconic-font/dist/css/material-design-iconic-font.css');
