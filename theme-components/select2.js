06e17f3d47cac70dea79e17f0e2a5a3df3e645f7
Max Loginov <info@maxloginov.com>
2017-01-14 17:42:25 -0800

    Updated jQuery and Select2 to next maxor versions

--- theme-components/select2.js
+++ theme-components/select2.js
@@ -1,6 +1,7 @@
 // select2 (jQuery replacement for select boxes) - works, fully functional
 require('./jquery.js');
+
 require('select2');
-require('select2/select2.css');
-require('select2/select2-bootstrap.css');
+require('select2/dist/css/select2.css');
+
 process.env.NODE_ENV !== 'production' && console.log('- select2 loaded');
